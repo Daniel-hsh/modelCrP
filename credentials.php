1cfea5bbc9b30d3decdc57fc7dd913fd389318c8
Daniel Henrique <danyhiro.seven@gmail.com>
2018-10-03 19:44:55 -0300

    Refatoracao de codigo - modulo de credenciais para acesso BD PHP

---
+++ credentials.php
@@ -0,0 +1,13 @@
+<?php
+    //AMBIENTE DE DESENVOLVIMENTO
+    define("hostName","127.0.0.1");
+    define("hostLogin","root");
+    define("hostPass","");
+    define("bdName","CRUDprocess");
+
+    //AMBIENTE DE MANUTEÇÃO
+
+    //AMBIENTE DE TESTE
+
+    //AMBIENTE DE PRODUÇÃO
+?>
